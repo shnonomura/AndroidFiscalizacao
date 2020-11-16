6538e82efc7f745bf5957c82779bba0e9b1599c4
Sergio <shnonomura@gmail.com>
2020-11-16 16:04:44 -0300

    versão 08 - 16/11/2020 - retirada a activity ItensOrdemServico.java. MainActivity e OrdensFiscal estão ok. Reiniciando a criação da activity "detalhes da OS selecionada". Ajustes no OrdensFiscal.java

---
+++ app/src/main/java/br/com/fiscalizacao/activity/DetailOs.java
@@ -0,0 +1,19 @@
+package br.com.fiscalizacao.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.fiscalizacao.R;
+
+public class DetailOs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_os);
+
+
+    } // fim do método onCreate
+
+} // fim da class DetailOs
